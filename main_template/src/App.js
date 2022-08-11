a9cbe43dbbe2ba91513b1d4121ce1abc12f482e3
ordaz00 <jlordaz@ucdavis.com>
2022-08-10 23:52:01 -0700

    tried to get the tabs to work

--- main_template/src/App.js
+++ main_template/src/App.js
@@ -1,32 +1,28 @@
 import {Card, StyledBody, StyledAction} from 'baseui/card';
 import {Button, KIND, SIZE, SHAPE} from 'baseui/button';
+import {Tabs, Tab, ORIENTATION} from 'baseui/tabs';
+import * as React from 'react';
 import './App.css';
 
 function App() {
+  const [activeKey, setActiveKey] = React.useState("0");
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <p>Title</p>
+          <p>Onyx Web Design</p>
         </div>
       </header>
-      <body>
-        <Card overrides={{Root: {style: {width: '328px'}}}}>
-          <StyledBody>
-            I was built using Baseweb
-          </StyledBody>
-          <StyledAction>
-            <Button
-              onClick={() => alert("click")}
-              kind={KIND.tertiary}
-              size={SIZE.default}
-              shape={SHAPE.default}
-            >
-              Hello
-            </Button>
-          </StyledAction>
-        </Card>
-      </body>
+     <Tabs
+       onChange={({ activeKey }) => {
+         setActiveKey(activeKey);
+       }}
+       activeKey={activeKey}
+     >
+       <Tab title="Tab Link 1">Content 1</Tab>
+       <Tab title="Tab Link 2">Content 2</Tab>
+       <Tab title="Tab Link 3">Content 3</Tab>
+     </Tabs>
     </div>
   );
 }
