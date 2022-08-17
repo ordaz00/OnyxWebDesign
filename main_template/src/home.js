d0893d6e26aa78dbdc3697545e97d9ed1fb9c3f7
ordaz00 <jlordaz@ucdavis.com>
2022-08-16 23:11:45 -0700

    got the font size to change, but then accidently broke it again

--- main_template/src/home.js
+++ main_template/src/home.js
@@ -1,18 +1,54 @@
 import * as React from 'react';
-import {Card, StyledBody} from 'baseui/card';
+import {Card, StyledBody, StyledAction} from 'baseui/card';
+import {Button, KIND, SIZE, SHAPE} from 'baseui/button';
+import { useStyletron } from 'styletron-react';
 
 function Home(){
+  let [css] = useStyletron();
     return(
         <div className='tabs' id='home'>
           <Card overrides={{Root:{style:{width:"475px", height: "475px", margin: "25px"}}}}>
-            <StyledBody>
-                Create Your Own or Choose from Our Templates
+            <StyledBody className={css({fontSize: "30px", marginTop: "100px"})}>
+              Create Your Own 
             </StyledBody>
+            <StyledBody className={css({fontSize: "30px", marginTop: "50px"})}>
+              or 
+            </StyledBody>
+            <StyledBody className={css({fontSize: "30px", marginTop: "50px"})}>
+              Choose From Our Templates
+            </StyledBody>
+            <StyledAction>
+              <Button
+                onClick={() => alert("click")}
+                kind={KIND.primary}
+                size={SIZE.default}
+                shape={SHAPE.default}
+                overrides={{BaseButton:{style:{width: "100%"}}}}
+                className={css({fontSize: "20px", marginTop: "50px"})}
+              >
+                Get Started
+              </Button>
+            </StyledAction>
           </Card>
           <Card overrides={{Root:{style:{width:"475px", height: "475px", margin: "25px"}}}}>
-            <StyledBody>
-              Gallery of Templates
+            <StyledBody className={css({fontSize: "50px", marginTop: "100px"})}>
+              Gallery of
             </StyledBody>
+            <StyledBody className={css({fontSize: "50px", marginTop: "50px"})}>
+              Templates
+            </StyledBody>
+            <StyledAction>
+              <Button
+                onClick={() => alert("click")}
+                kind={KIND.primary}
+                size={SIZE.default}
+                shape={SHAPE.default}
+                overrides={{BaseButton:{style:{width: "100%"}}}}
+                className={css({fontSize: "20px", marginTop: "100px"})}
+              >
+                View Templates
+              </Button>
+            </StyledAction>
           </Card>
         </div>
     );
--- main_template/src/templates.js
+++ main_template/src/templates.js
@@ -1,57 +1,60 @@
 import * as React from 'react';
-import {Card} from 'baseui/card';
+import {Card, StyledAction} from 'baseui/card';
+import {Button, KIND, SIZE, SHAPE} from 'baseui/button';
+import { useStyletron } from 'styletron-react';
 
 function Templates(){
-    const CONCERTS = [
-        {
-          eventDateName: "Jón Jónsson og Friðrik Dór - fjölskyldutónleikar",
-          name: "Tónleikar",
-          dateOfShow: "2018-12-15T14:00:00",
-          eventHallName: "Bæjarbíó (Hafnarfirði)",
-          imageSource:
-            "https://d30qys758zh01z.cloudfront.net/images/medium/1.10700.jpg"
-        },
-        {
-          eventDateName: "Jón Jónsson og Friðrik Dór - fjölskyldutónleikar",
-          name: "Tónleikar-UPPSELT",
-          dateOfShow: "2018-12-15T16:00:00",
-          eventHallName: "Bæjarbíó (Hafnarfirði)",
-          imageSource:
-            "https://d30qys758zh01z.cloudfront.net/images/medium/1.10700.jpg"
-        },
-        {
-          eventDateName: "Hera Björk - Ilmur af jólum - Í borg og bæ",
-          name: "Hólmavík",
-          dateOfShow: "2018-12-15T17:00:00",
-          eventHallName: "Hólmavíkurkirkja",
-          imageSource:
-            "https://d30qys758zh01z.cloudfront.net/images/medium/1.10648.jpg"
-        },
-        {
-          eventDateName: "Hátíðartónleikar Eyþórs Inga",
-          name: "Víðistaðakirkja",
-          dateOfShow: "2018-12-15T20:00:00",
-          eventHallName: "Víðistaðakirkja (Hafnarfirði)",
-          imageSource:
-            "https://d30qys758zh01z.cloudfront.net/images/medium/1.10630.jpg"
-        },
-        {
-          eventDateName: "Jólin til þín",
-          name: "Höfn",
-          dateOfShow: "2018-12-15T20:00:00",
-          eventHallName: "Íþróttahúsið á Höfn",
-          imageSource:
-            "https://d30qys758zh01z.cloudfront.net/images/medium/1.10647.jpg"
-        },
-        {
-          eventDateName: "Jólalögin þeirra",
-          name: "Tónleikar",
-          dateOfShow: "2018-12-15T21:00:00",
-          eventHallName: "Hendur í Höfn",
-          imageSource:
-            "https://d30qys758zh01z.cloudfront.net/images/medium/1.10687.jpg"
-        }
-      ];
+  let [css] = useStyletron();
+  const CONCERTS = [
+    {
+      eventDateName: "Jón Jónsson og Friðrik Dór - fjölskyldutónleikar",
+      name: "Tónleikar",
+      dateOfShow: "2018-12-15T14:00:00",
+      eventHallName: "Bæjarbíó (Hafnarfirði)",
+      imageSource:
+        "https://d30qys758zh01z.cloudfront.net/images/medium/1.10700.jpg"
+    },
+    {
+      eventDateName: "Jón Jónsson og Friðrik Dór - fjölskyldutónleikar",
+      name: "Tónleikar-UPPSELT",
+      dateOfShow: "2018-12-15T16:00:00",
+      eventHallName: "Bæjarbíó (Hafnarfirði)",
+      imageSource:
+        "https://d30qys758zh01z.cloudfront.net/images/medium/1.10700.jpg"
+    },
+    {
+      eventDateName: "Hera Björk - Ilmur af jólum - Í borg og bæ",
+      name: "Hólmavík",
+      dateOfShow: "2018-12-15T17:00:00",
+      eventHallName: "Hólmavíkurkirkja",
+      imageSource:
+        "https://d30qys758zh01z.cloudfront.net/images/medium/1.10648.jpg"
+    },
+    {
+      eventDateName: "Hátíðartónleikar Eyþórs Inga",
+      name: "Víðistaðakirkja",
+      dateOfShow: "2018-12-15T20:00:00",
+      eventHallName: "Víðistaðakirkja (Hafnarfirði)",
+      imageSource:
+        "https://d30qys758zh01z.cloudfront.net/images/medium/1.10630.jpg"
+    },
+    {
+      eventDateName: "Jólin til þín",
+      name: "Höfn",
+      dateOfShow: "2018-12-15T20:00:00",
+      eventHallName: "Íþróttahúsið á Höfn",
+      imageSource:
+        "https://d30qys758zh01z.cloudfront.net/images/medium/1.10647.jpg"
+    },
+    {
+      eventDateName: "Jólalögin þeirra",
+      name: "Tónleikar",
+      dateOfShow: "2018-12-15T21:00:00",
+      eventHallName: "Hendur í Höfn",
+      imageSource:
+        "https://d30qys758zh01z.cloudfront.net/images/medium/1.10687.jpg"
+    }
+  ];
     return(
         <div id='templates'>
             {CONCERTS.map(concert => (
@@ -65,6 +68,18 @@ function Templates(){
                         }
                     }}
                 >
+                  <StyledAction>
+                    <Button
+                      onClick={() => alert("click")}
+                      kind={KIND.primary}
+                      size={SIZE.default}
+                      shape={SHAPE.default}
+                      overrides={{BaseButton:{style:{width: "100%"}}}}
+                      className={css({fontSize: "20px"})}
+                    >
+                      Select
+                    </Button>
+                  </StyledAction>
                 </Card>
             ))}
             <div id='selfDesignSlogan'>
