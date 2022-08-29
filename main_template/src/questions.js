b3b5328d30cca42f877b3246feb3a14cd6024f7a
ordaz00 <jlordaz@ucdavis.com>
2022-08-28 23:39:34 -0700

    began working on the questions, have the initial set up, need to add text box and other functionality

--- main_template/src/questions.js
+++ main_template/src/questions.js
@@ -5,11 +5,26 @@ import { useStyletron } from 'styletron-react';
 
 
 function Questions(props){
+  let [question, changeQuestion] = React.useState(0)
   let [css] = useStyletron();
+
+  let questions = ["Hi", "Hello", "Wassup"]
 
   function pageChange(){
     props.updatePage("fileUpload");
   }
+
+  function back(){
+    if(question > 0){
+      changeQuestion(question - 1);
+    }
+}
+
+function next(){
+  if(question < questions.length - 1){
+    changeQuestion(question + 1);
+  }
+}
 
   return(
     <div id='questions'>
@@ -18,19 +33,19 @@ function Questions(props){
         className={css({width: "500px", height: "500px"})}
      >
        <StyledBody className={css({margin: "50px"})}>
-         Proin ut dui sed metus pharetra hend rerit vel non mi.
-         Nulla ornare faucibus ex, non facilisis nisl. Proin ut
-         dui sed metus pharetra hend rerit vel non mi. Nulla
-         ornare faucibus ex, non facilisis nisl.
+        {questions[question]}
        </StyledBody>
        <StyledAction>
-         <Button
-           overrides={{
-             BaseButton: { style: { width: "100%" } }
-           }}
-           className={css({margin: "50px"})}
-         >
-           Button Label
+       </StyledAction>
+       <StyledAction className={css({position: "absolute", top: "135%", left: "555px"})}>
+        <Button onClick={back} >
+          Back
+        </Button>
+        <Button className={css({margin: "0px 100px 0px 100px"})}>
+          Continue
+        </Button>
+         <Button onClick={next}>
+          Next
          </Button>
        </StyledAction>
      </Card>
